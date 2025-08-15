05fcc6530d882b9dee43768dc97f6e9a1b7d6d89
nukelash <lnash016@gmail.com>
2025-08-15 17:22:35 -0400

    controls totally updated

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,12 @@
 // float saw, freq, output;
 
 oscillator my_osc;
+lfo my_lfo;
 filter my_filt;
 mixer my_mixer;
 envelope_generator my_envelope;
 vca my_vca;
+mult my_mult;
 sequencer my_sequencer;
 patch_manager my_patch_bay;
 
@@ -37,9 +39,11 @@ void callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 f
     for (int i = 0; i < frameCount; i++) {
 
         my_osc.process();
+        my_lfo.process();
         my_filt.process();
         my_envelope.process();
         my_vca.process();
+        my_mult.process();
         my_sequencer.process();
         out[i] = my_mixer.process();
 
@@ -67,10 +71,12 @@ void gui_loop() {
             ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
             my_osc.draw();
+            my_lfo.draw();
             my_filt.draw();
             my_mixer.draw();
             my_envelope.draw();
             my_vca.draw();
+            my_mult.draw();
             my_sequencer.draw();
             my_patch_bay.draw();
 
@@ -84,9 +90,11 @@ int main() {
     int user_data;
 
     my_osc.init(48000, &my_patch_bay);
+    my_lfo.init(48000, &my_patch_bay);
     my_filt.init(48000, &my_patch_bay);
     my_envelope.init(48000);
     my_vca.init();
+    my_mult.init();
     my_sequencer.init(48000, 90);
     my_mixer.init();
 
@@ -95,16 +103,24 @@ int main() {
     my_patch_bay.add("my_osc_sqr", &my_osc._audio_sqr_out);
     my_patch_bay.add("my_osc_freq", &my_osc._audio_frequency_mod);
 
-    my_patch_bay.add("my_osc_lfo_tri", &my_osc._lfo_tri_out);
-    my_patch_bay.add("my_osc_lfo_saw", &my_osc._lfo_saw_out);
-    my_patch_bay.add("my_osc_lfo_sqr", &my_osc._lfo_sqr_out);
+    my_patch_bay.add("my_osc_lfo_tri", &my_lfo._lfo_tri_out);
+    my_patch_bay.add("my_osc_lfo_saw", &my_lfo._lfo_saw_out);
+    my_patch_bay.add("my_osc_lfo_sqr", &my_lfo._lfo_sqr_out);
 
     my_patch_bay.add("env_trigger", &my_envelope._trigger);
     my_patch_bay.add("env_out", &my_envelope._output);
 
-    my_patch_bay.add("vca_in_1", &my_vca._in_1);
-    my_patch_bay.add("vca_in_2", &my_vca._in_2);
-    my_patch_bay.add("vca_out", &my_vca._out_1);
+    my_patch_bay.add("vca_in_a_1", &my_vca._in_a1);
+    my_patch_bay.add("vca_in_a_2", &my_vca._in_a2);
+    my_patch_bay.add("vca_out_a", &my_vca._out_a1);
+    my_patch_bay.add("vca_in_b_1", &my_vca._in_b1);
+    my_patch_bay.add("vca_in_b_2", &my_vca._in_b2);
+    my_patch_bay.add("vca_out_b", &my_vca._out_b1);
+
+    my_patch_bay.add("mult_in", &my_mult._in);
+    my_patch_bay.add("mult_out1", &my_mult._out1);
+    my_patch_bay.add("mult_out2", &my_mult._out2);
+    my_patch_bay.add("mult_out3", &my_mult._out3);
 
     my_patch_bay.add("sequencer_trig_out", &my_sequencer._trig);
     my_patch_bay.add("sequencer_cv_out", &my_sequencer._cv);
--- src/modules.h
+++ src/modules.h
@@ -16,23 +16,24 @@ public:
 
     void init(std::string label, Vector2 position) {
 
-        float width = 40;
-        float height = 100;
+        // float width = 40;
+        // float height = 100;
 
-        float x = position.x + (0.5*width);
-        float y = position.y + (0.7*height);
-        _circle_position = {x, y};
-        _radius = 20;
+        // float x = position.x + (0.5*width);
+        // float y = position.y + (0.7*height);
+        _radius = 15;
+        _circle_position = {position.x+_radius, position.y+_radius};
+        
 
-        y = position.y + (0.3*height);
-        _label_position = {position.x, y};
+        // y = position.y + (0.3*height);
+        _label_position = {position.x, position.y-10};
         _label = label; 
     }
 
     void draw() {
         DrawCircleV(_circle_position, _radius, RED);
         DrawCircleV(_circle_position, _radius-3, GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-        DrawText(_label.c_str(), _label_position.x, _label_position.y, 16, BLACK);
+        DrawText(_label.c_str(), _label_position.x, _label_position.y, 11, BLACK);
     }
 
     std::string _label;
@@ -255,31 +256,19 @@ public:
         _audio_saw.SetWaveform(daisysp::Oscillator::WAVE_SAW);
         _audio_sqr.SetWaveform(daisysp::Oscillator::WAVE_SQUARE);
 
-        _lfo_frequency = 0.5;
-        for (auto i : _lfo_oscillators) {
-            i->Init(sample_rate);
-            i->SetFreq(_lfo_frequency);
-        }
-        _lfo_tri.SetWaveform(daisysp::Oscillator::WAVE_TRI);
-        _lfo_saw.SetWaveform(daisysp::Oscillator::WAVE_SAW);
-        _lfo_sqr.SetWaveform(daisysp::Oscillator::WAVE_SQUARE);
+        _module_box.x = 30;
+        _module_box.y = 45;
+        _module_box.width = 255;
+        _module_box.height = 135;
 
-        _module_box.x = 20;
-        _module_box.y = 20;
-        _module_box.width = 150;
-        _module_box.height = 300;
+        _audio_frequency_mod.gui.init("Freq Mod", {_module_box.x+82.5f, _module_box.y+82.5f});
 
-        _audio_frequency_mod.gui.init("Freq Mod", {_module_box.x+5, _module_box.y+30});
+        _audio_tri_out.gui.init("Tri", {_module_box.x+202.5f, _module_box.y+22.5f});
+        _audio_saw_out.gui.init("Saw", {_module_box.x+202.5f, _module_box.y+82.5f});
+        _audio_sqr_out.gui.init("Sqr", {_module_box.x+142.5f, _module_box.y+82.5f});
 
-        _audio_tri_out.gui.init("Tri", {_module_box.x+5, _module_box.y+80});
-        _audio_saw_out.gui.init("Saw", {_module_box.x+50, _module_box.y+80});
-        _audio_sqr_out.gui.init("Sqr", {_module_box.x+95, _module_box.y+80});
-
-        _lfo_tri_out.gui.init("Tri", {_module_box.x+5, _module_box.y+180});
-        _lfo_saw_out.gui.init("Saw", {_module_box.x+50, _module_box.y+180});
-        _lfo_sqr_out.gui.init("Sqr", {_module_box.x+95, _module_box.y+180});
-
-        _frequency_knob = new knob({_module_box.x + 10, _module_box.y + 80}, 40, &_audio_frequency, 20, 500);
+        float freq_radius = 30;
+        _frequency_knob = new knob({_module_box.x + freq_radius + 22.5f, _module_box.y + freq_radius + 22.5f},freq_radius, &_audio_frequency, 20, 500);
     }
 
     void draw() {
@@ -291,11 +280,9 @@ public:
         float x_index = _module_box.x + x_pad;
         GuiGroupBox(_module_box, "VCO");
 
-        GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Frequency", "", &_audio_frequency, 20, 500);
+        //GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Frequency", "", &_audio_frequency, 20, 500);
+        _frequency_knob->draw();
         y_index += 160;
-
-        GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "LFO Freq", "", &_lfo_frequency, 0.1, 50);
-        y_index += y_pad;
 
         _audio_frequency_mod.gui.draw();
 
@@ -303,9 +290,7 @@ public:
             i->gui.draw();
         }
 
-        for (auto i : _lfo_outs) {
-            i->gui.draw();
-        }
+        
 
         _frequency_knob->draw();
     }
@@ -316,13 +301,8 @@ public:
             i->SetFreq(_audio_frequency + (100*_audio_frequency_mod.val()));
         }
 
-        for (auto i : _lfo_oscillators) {
-            i->SetFreq(_lfo_frequency);
-        }
-
         for (int i = 0; i < _audio_oscillators.size(); i++) {
             _audio_outs[i]->value = _audio_oscillators[i]->Process();
-            _lfo_outs[i]->value = _lfo_oscillators[i]->Process();
         }
 
     }
@@ -337,11 +317,6 @@ public:
     patch_source _audio_sqr_out;
     std::vector<patch_source*> _audio_outs = {&_audio_tri_out, &_audio_saw_out, &_audio_sqr_out};
 
-    patch_source _lfo_tri_out;
-    patch_source _lfo_saw_out;
-    patch_source _lfo_sqr_out;
-    std::vector<patch_source*> _lfo_outs = {&_lfo_tri_out, &_lfo_saw_out, &_lfo_sqr_out};
-
     patch_destination _audio_frequency_mod;
 
 private:
@@ -353,16 +328,80 @@ private:
     float _audio_frequency;
     //int _wavetype;
 
-    daisysp::Oscillator _lfo_tri;
-    daisysp::Oscillator _lfo_saw;
-    daisysp::Oscillator _lfo_sqr;
-    std::vector<daisysp::Oscillator*> _lfo_oscillators = {&_lfo_tri, &_lfo_saw, &_lfo_sqr};
-    float _lfo_frequency;
+    
 
     //int _out_counter;
     
 
     //patch_destinations* _patch_destinations;
+
+    patch_manager* _patch_bay;
+
+    Rectangle _module_box;
+
+    knob* _frequency_knob;
+};
+
+class lfo {
+public:
+    lfo() {}
+    ~lfo() {}
+
+    void init(float sample_rate, patch_manager* patch_bay) {
+        _patch_bay = patch_bay;
+
+        _module_box = {30, 187.5, 180, 112.5};
+
+        _lfo_frequency = 0.5;
+        for (auto i : _lfo_oscillators) {
+            i->Init(sample_rate);
+            i->SetFreq(_lfo_frequency);
+        }
+        _lfo_tri.SetWaveform(daisysp::Oscillator::WAVE_TRI);
+        _lfo_saw.SetWaveform(daisysp::Oscillator::WAVE_SAW);
+        _lfo_sqr.SetWaveform(daisysp::Oscillator::WAVE_SQUARE);
+
+        _lfo_tri_out.gui.init("Tri", {_module_box.x+135, _module_box.y+15});
+        _lfo_saw_out.gui.init("Saw", {_module_box.x+135, _module_box.y+67.f});
+        _lfo_sqr_out.gui.init("Sqr", {_module_box.x+75, _module_box.y+67.5f});
+
+        float radius = 22.5f;
+        _frequency_knob  = new knob({_module_box.x+67.5f+radius, _module_box.y+7.5f+radius}, radius, &_lfo_frequency, 0.01, 50);
+    }
+
+    void draw() {
+
+        GuiGroupBox(_module_box, "LFO");
+
+        _frequency_knob->draw();
+
+        for (auto i : _lfo_outs) {
+            i->gui.draw();
+        }
+    }
+
+    float process() {
+        for (auto i : _lfo_oscillators) {
+            i->SetFreq(_lfo_frequency);
+        }
+
+        for (int i = 0; i < _lfo_oscillators.size(); i++) {
+            _lfo_outs[i]->value = _lfo_oscillators[i]->Process();
+        }
+    }
+
+    patch_source _lfo_tri_out;
+    patch_source _lfo_saw_out;
+    patch_source _lfo_sqr_out;
+    std::vector<patch_source*> _lfo_outs = {&_lfo_tri_out, &_lfo_saw_out, &_lfo_sqr_out};
+
+private:
+
+    daisysp::Oscillator _lfo_tri;
+    daisysp::Oscillator _lfo_saw;
+    daisysp::Oscillator _lfo_sqr;
+    std::vector<daisysp::Oscillator*> _lfo_oscillators = {&_lfo_tri, &_lfo_saw, &_lfo_sqr};
+    float _lfo_frequency;
 
     patch_manager* _patch_bay;
 
@@ -384,14 +423,22 @@ public:
         _resonance = 0.5;
         _filter.SetFreq(_frequency);
 
-        _module_box = {190, 20, 150, 300};
+        _module_box = {292.5f, 45, 195, 135};
 
-        _in.gui.init("input", {_module_box.x+5, _module_box.y+60});
-        _out.gui.init("output", {_module_box.x+60, _module_box.y+60});
+        _in.gui.init("input", {_module_box.x+22.5f, _module_box.y+82.5f});
+        _out.gui.init("output", {_module_box.x+142.5f, _module_box.y+82.5f});
+
+        float radius = 22.5f;
+        _cutoff_knob = new knob({_module_box.x+45+radius, _module_box.y+22.5f+radius}, radius, &_frequency, 20, 15000);
+
+        _resonance_knob = new knob({_module_box.x+105+radius, _module_box.y+22.5f+radius}, radius, &_resonance, 0.0, 1.5f);
 
     }
 
     float process() {
+
+        _filter.SetFreq(_frequency);
+        _filter.SetRes(_resonance);
 
         _filter.Process(_in.val());
 
@@ -407,18 +454,20 @@ public:
         float x_index = _module_box.x + x_pad;
         GuiGroupBox(_module_box, "VCF");
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Frequency", "", &_frequency, 20, 15000)) {
-            _filter.SetFreq(_frequency);
-        }
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Frequency", "", &_frequency, 20, 15000)) {
+        //     _filter.SetFreq(_frequency);
+        // }
         y_index += y_pad;
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Resonance", "", &_resonance, 0.0, 2.0)) {
-            _filter.SetRes(_resonance);
-        }
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Resonance", "", &_resonance, 0.0, 2.0)) {
+        //     _filter.SetRes(_resonance);
+        // }
         y_index += y_pad;
 
         _in.gui.draw();
         _out.gui.draw();
+        _cutoff_knob->draw();
+        _resonance_knob->draw();
     }
 
     patch_destination _in;
@@ -432,6 +481,9 @@ private:
     patch_manager* _patch_bay;
 
     Rectangle _module_box;
+
+    knob* _cutoff_knob;
+    knob* _resonance_knob;
 };
 
 class envelope_generator {
@@ -442,17 +494,25 @@ public:
     void init(float sample_rate) {
         _env.Init(sample_rate);
 
-        _module_box = {380, 20, 150, 300};
+        _module_box = {330,187.5, 157.5, 112.5};
 
         _attack = 0.1;
         _decay = 0.5;
         _last_trig_value = 0.0;
 
-        _trigger.gui.init("trigger", {_module_box.x+5, _module_box.y+200});
-        _output.gui.init("env out", {_module_box.x+50, _module_box.y+200});
+        _trigger.gui.init("trigger", {_module_box.x+15, _module_box.y+67.5f});
+        _output.gui.init("env out", {_module_box.x+112.5f, _module_box.y+41.25f});
+
+        float radius = 22.5f;
+        _attack_knob = new knob({_module_box.x+60+radius, _module_box.y+7.5f+radius}, radius, &_attack, 0.01, 1.0);
+        _decay_knob = new knob({_module_box.x+60+radius, _module_box.y+60+radius}, radius, &_decay, 0.01, 2.0);
     }
 
     void process() {
+
+        _env.SetTime(daisysp::ADENV_SEG_ATTACK, _attack);
+        _env.SetTime(daisysp::ADENV_SEG_DECAY, _decay);
+
         if(_trigger.val() > _last_trig_value) {
             _env.Trigger();
         }
@@ -469,23 +529,24 @@ public:
         float x_index = _module_box.x + x_pad;
         GuiGroupBox(_module_box, "Envelope");
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Attack", "", &_attack, 0.01, 2.0)) {
-            _env.SetTime(daisysp::ADENV_SEG_ATTACK, _attack);
-        }
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Attack", "", &_attack, 0.01, 2.0)) {
+        //     _env.SetTime(daisysp::ADENV_SEG_ATTACK, _attack);
+        // }
         y_index += y_pad;
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Decay", "", &_decay, 0.01, 2.0)) {
-            _env.SetTime(daisysp::ADENV_SEG_DECAY, _decay);
-        }
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Decay", "", &_decay, 0.01, 2.0)) {
+        //     _env.SetTime(daisysp::ADENV_SEG_DECAY, _decay);
+        // }
         y_index += y_pad;
 
-        if (GuiButton((Rectangle){x_index, y_index, _module_box.width - (2*x_pad), 20}, "Trigger")) {
+        if (GuiButton((Rectangle){_module_box.x+15, _module_box.y+15, 30, 30}, "Trigger")) {
             _env.Trigger();
         }
 
         _trigger.gui.draw();
         _output.gui.draw();
-        //std::cout << _env.GetCurrentSegment() <<std::endl;
+        _attack_knob->draw();
+        _decay_knob->draw();
     }
 
     patch_destination _trigger;
@@ -500,6 +561,47 @@ private:
 
     Rectangle _module_box;
 
+    knob* _attack_knob;
+    knob* _decay_knob;
+};
+
+class mult {
+public:
+    mult() {}
+    ~mult(){}
+
+    void init() {
+        _module_box = {217.5, 187.5, 105, 112.5};
+
+        _in.gui.init("in", {_module_box.x+15, _module_box.y+15});
+        _out1.gui.init("out", {_module_box.x+60, _module_box.y+15});
+        _out2.gui.init("out", {_module_box.x+15, _module_box.y+67.5f});
+        _out3.gui.init("out", {_module_box.x+60, _module_box.y+67.5f});
+    }
+
+    void draw() {
+
+        GuiGroupBox(_module_box, "Mult");
+
+        _in.gui.draw();
+        _out1.gui.draw();
+        _out2.gui.draw();
+        _out3.gui.draw();
+    }
+
+    void process() {
+        _out1.value = _in.val();
+        _out2.value = _in.val();
+        _out3.value = _in.val();
+    }
+
+    patch_destination _in;
+    patch_source _out1;
+    patch_source _out2;
+    patch_source _out3;
+
+private:
+    Rectangle _module_box;
 };
 
 class vca {
@@ -508,26 +610,38 @@ public:
     ~vca() {}
 
     void init() {
-        _module_box = {600, 20, 100, 300};
-        _in_1.gui.init("in1", {_module_box.x+5, _module_box.y+30});
-        _in_2.gui.init("in2", {_module_box.x+5, _module_box.y+80});
-        _out_1.gui.init("out1", {_module_box.x+5, _module_box.y+130});
+        _module_box = {495, 45, 97.5f, 255};
+        _in_a1.gui.init("ina1", {_module_box.x+15, _module_box.y+22.5f});
+        _in_a2.gui.init("ina2", {_module_box.x+15, _module_box.y+82.5f});
+        _out_a1.gui.init("outa1", {_module_box.x+52.5f, _module_box.y+52.5f});
+
+        _in_b1.gui.init("inb1", {_module_box.x+15, _module_box.y+142.5f});
+        _in_b2.gui.init("inb2", {_module_box.x+15, _module_box.y+202.5f});
+        _out_b1.gui.init("outb1", {_module_box.x+52.5f, _module_box.y+172.5f});
     }
 
     void process() {
-        _out_1.value = _in_1.val() * _in_2.val();
+        _out_a1.value = _in_a1.val() * _in_a2.val();
+        _out_b1.value = _in_b1.val() * _in_b2.val();
     }
 
     void draw() {
         GuiGroupBox(_module_box, "VCA");
-        _in_1.gui.draw();
-        _in_2.gui.draw();
-        _out_1.gui.draw();
+        _in_a1.gui.draw();
+        _in_a2.gui.draw();
+        _out_a1.gui.draw();
+        _in_b1.gui.draw();
+        _in_b2.gui.draw();
+        _out_b1.gui.draw();
     }
 
-    patch_destination _in_1;
-    patch_destination _in_2;
-    patch_source _out_1;
+    patch_destination _in_a1;
+    patch_destination _in_a2;
+    patch_source _out_a1;
+
+    patch_destination _in_b1;
+    patch_destination _in_b2;
+    patch_source _out_b1;
 
 private:
     Rectangle _module_box;
@@ -540,15 +654,27 @@ public:
 
     void init(float sample_rate, float tempo) {
 
-        _module_box = {20, 330, 900, 100};
-        _cv.gui.init("CV Out", {_module_box.x + 5, _module_box.y + 5});
-        _trig.gui.init("Gate Out", {_module_box.x + 65, _module_box.y + 5});
+        _module_box = {30, 307.5f, 562.5f, 97.5f};
+        _cv.gui.init("CV Out", {_module_box.x + 82.5f, _module_box.y + 52.5f});
+        _trig.gui.init("Gate Out", {_module_box.x + 82.5f, _module_box.y + 15});
+        _tempo = tempo;
 
-        _metronome.Init(tempo/60.0f*_num_steps, sample_rate); //multiplying by _num_steps should make it pulse once per step
+        _metronome.Init(_tempo/60.0f*_num_steps, sample_rate); //multiplying by _num_steps should make it pulse once per step
         _step = 0;
+
+        float radius = 22.5f;
+        _tempo_knob = new knob({_module_box.x+15+radius, _module_box.y+26.25f+radius},radius, &_tempo, 30, 160);
+
+        radius = 18.75;
+        float gap = 15;
+        for (int i = 0; i < _num_steps; i++) {
+            _step_knobs[i] = new knob({_module_box.x+146.25f+radius+(i*(radius+radius+gap)), _module_box.y+52.5f+radius}, radius, &_cv_pattern[i], -1.0f, 1.0f);
+        }
     }
 
     void process() {
+
+        _metronome.SetFreq(_tempo/60.0f*_num_steps);
 
         int new_step = _metronome.Process();
         _trig.value = _trig_pattern[_step] * new_step;
@@ -568,11 +694,11 @@ public:
         GuiGroupBox(_module_box, "Sequencer");
 
         for(int i = 0; i < _num_steps; i++) {
-            Rectangle bounds = {_module_box.x+150+(i*50), _module_box.y + 10, 20, 20};
+            Rectangle bounds = {_module_box.x+150+(i*52.5f), _module_box.y + 7.5f, 30, 30};
             GuiCheckBox(bounds, "", &_trig_pattern[i]);
             bounds.y += 30;
             bounds.width = 40;
-            GuiSlider(bounds, "", "", &_cv_pattern[i], -1.0f, 1.0f);
+            //GuiSlider(bounds, "", "", &_cv_pattern[i], -1.0f, 1.0f);
 
         }
 
@@ -582,6 +708,10 @@ public:
 
         _cv.gui.draw();
         _trig.gui.draw();
+        _tempo_knob->draw();
+        for (int i= 0; i < _num_steps; i++){
+            _step_knobs[i]->draw();
+        }
     }
 
     void set_trig(int step, bool value) {
@@ -601,8 +731,12 @@ private:
     int _step;
     bool _trig_pattern[_num_steps] = {0};
     float _cv_pattern[_num_steps] = {0.0f};
+    float _tempo;
 
     Rectangle _module_box;
+
+    knob* _tempo_knob;
+    knob* _step_knobs[_num_steps];
 };
 
 class mixer {
@@ -614,11 +748,15 @@ public:
         _gain_1= 0.5;
         _gain_2 = 0.5;
 
-        _module_box = {700, 20, 150, 300};
+        _module_box = {600, 45, 90, 360};
 
-        _in_1.gui.init("Audio 1", {_module_box.x+5, _module_box.y+200});
+        _in_1.gui.init("Audio 1", {_module_box.x+7.5f, _module_box.y+22.5f});
 
-        _in_2.gui.init("Audio 2", {_module_box.x+60, _module_box.y+200});
+        _in_2.gui.init("Audio 2", {_module_box.x+7.5f, _module_box.y+112.5f});
+
+        float radius = 22.5;
+        _gain_1_knob = new knob({_module_box.x+37.5f+radius, _module_box.y+45+radius}, radius, &_gain_1, 0.0, 1.0);
+        _gain_2_knob = new knob({_module_box.x+37.5f+radius, _module_box.y+135+radius}, radius, &_gain_2, 0.0, 1.0);
     }
 
     float process() {
@@ -634,14 +772,16 @@ public:
         float x_index = _module_box.x + x_pad;
         GuiGroupBox(_module_box, "Mixer");
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Gain 1", "", &_gain_1, 0.0, 1.0)) {}
-        y_index += y_pad;
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Gain 1", "", &_gain_1, 0.0, 1.0)) {}
+        // y_index += y_pad;
 
-        if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Gain 2", "", &_gain_2, 0.0, 1.0)) {}
-        y_index += y_pad;
+        // if (GuiSlider((Rectangle) {x_index, y_index, _module_box.width - (2*x_pad), 20}, "Gain 2", "", &_gain_2, 0.0, 1.0)) {}
+        // y_index += y_pad;
 
         _in_1.gui.draw();
         _in_2.gui.draw();
+        _gain_1_knob->draw();
+        _gain_2_knob->draw();
     }
 
     patch_destination _in_1;
@@ -652,4 +792,7 @@ private:
     float _gain_2;
 
     Rectangle _module_box;
+
+    knob* _gain_1_knob;
+    knob* _gain_2_knob;
 };
